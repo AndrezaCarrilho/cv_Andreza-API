f9bca8afdab0f9e252ac81fb177f4529b7aebf1d
AndrezaCarrilho <108102040+AndrezaCarrilho@users.noreply.github.com>
2025-11-28 18:55:19 +0000

    fix: atualização fim do codigo do index pois o postman havia parado de funcionar

--- src/index.js
+++ src/index.js
@@ -210,5 +210,10 @@ app.delete("/education/:edu_id", async (req, res) => {
   }
 });
 
- 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
 export default app;
